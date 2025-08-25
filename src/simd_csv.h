338a0df6ff4865a11501cd9e6843998c4b2fb41c
Al <albarrentine@gmail.com>
2025-08-25 01:35:57 -0400

    [csv/simd] finished initial implementation. Fixed bug in original simdcsv implementation on last line (need to do one final loop if the length of the last block does not align to a 64-byte boundary). Currently about 5x faster than the naive implementation using a dynamic array in approximatly the same way (appending once every 64 indices). This implementation gets somewhere around 1.6GB/s on my Intel Mac.

--- src/simd_csv.h
+++ src/simd_csv.h
@@ -7,6 +7,7 @@
 
 #include "aligned/aligned.h"
 #include "bit_utils/bit_utils.h"
+#include "index_array/uint32_index_array.h"
 #include "really_inline/really_inline.h"
 #include "simde_avx2/avx2.h"
 #include "simde_clmul/clmul.h"
@@ -34,12 +35,8 @@ really_inline uint64_t simd_csv_cmp_mask_against_input(simd_csv_input_t input, u
     return res_0 | (res_1 << 32);
 }
 
-really_inline uint64_t simd_csv_quote_mask(const uint8_t *buffer, uint64_t *prev_inside_quote) {
-    if (buffer == NULL) return 0;
-
-    uint64_t quote_bits = simd_csv_cmp_mask_against_input(
-        simd_csv_fill_input(buffer), '"'
-    );
+really_inline uint64_t simd_csv_quote_mask(simd_csv_input_t input, uint64_t *prev_inside_quote) {
+    uint64_t quote_bits = simd_csv_cmp_mask_against_input(input, '"');
 
     /* At this point the quote mask for a given input will logically look like:
      * Input:      ,"foo""bar"baz\n
@@ -63,6 +60,167 @@ really_inline uint64_t simd_csv_quote_mask(const uint8_t *buffer, uint64_t *prev
     return quote_mask;
 }
 
+typedef struct {
+    uint8_t delimiter;
+    bool crlf;
+    bool has_header;
+    bool trim_whitespace;
+    bool allow_quoted_newlines;
+} simd_csv_options_t;
 
+really_inline void simd_csv_flatten_bits(uint32_t *base_ptr, uint32_t *base_idx, uint32_t idx, uint64_t bits) {
+    if (bits != 0ULL) {
+        uint32_t base = *base_idx;
+        uint32_t cnt = popcount(bits);
+        uint32_t next_base = base + cnt;
+        base_ptr[base + 0] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        base_ptr[base + 1] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        base_ptr[base + 2] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        base_ptr[base + 3] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        base_ptr[base + 4] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        base_ptr[base + 5] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        base_ptr[base + 6] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        base_ptr[base + 7] = idx + ctz(bits);
+        bits = bits & (bits - 1);
+        if (cnt > 8) {
+            base_ptr[base + 8] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+            base_ptr[base + 9] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+            base_ptr[base + 10] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+            base_ptr[base + 11] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+            base_ptr[base + 12] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+            base_ptr[base + 13] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+            base_ptr[base + 14] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+            base_ptr[base + 15] = idx + ctz(bits);
+            bits = bits & (bits - 1);
+        }
+        if (cnt > 16) {
+            base += 16;
+            do {
+                base_ptr[base] = idx + ctz(bits);
+                bits = bits & (bits - 1);
+                base++;
+            } while (bits != 0ULL);
+        }
+        *base_idx = next_base;
+    }
+}
+
+
+bool simd_csv_find_indexes(const uint8_t *buffer, size_t len, uint32_index_array *csv, simd_csv_options_t options) {
+    if (buffer == NULL || csv == NULL) return false;
+    uint64_t prev_inside_quote = 0ULL;
+
+    uint64_t prev_cr_end = 0ULL;
+    size_t len_last_64 = len < 64 ? 0 : len - (len % 64);
+    size_t idx = 0;
+
+    #define SIMD_CSV_BUFFER_SIZE 4
+    if (len_last_64 > 64 * SIMD_CSV_BUFFER_SIZE) {
+        uint64_t fields[SIMD_CSV_BUFFER_SIZE] = {0ULL};
+        uint32_t buffer_indexes[SIMD_CSV_BUFFER_SIZE * 64] = {0};
+        for (idx = 0; idx < len_last_64 - (len_last_64 % (64 * SIMD_CSV_BUFFER_SIZE)); idx += 64 * SIMD_CSV_BUFFER_SIZE) {
+            for (size_t b = 0; b < SIMD_CSV_BUFFER_SIZE; b++) {
+                size_t internal_idx = 64 * b + idx;
+                #ifndef _MSC_VER
+                __builtin_prefetch(buffer + internal_idx + 128);
+                #endif
+
+                simd_csv_input_t input = simd_csv_fill_input(buffer + internal_idx);
+                uint64_t quote_mask = simd_csv_quote_mask(input, &prev_inside_quote);
+                uint64_t delim = simd_csv_cmp_mask_against_input(input, options.delimiter);
+                uint64_t end = 0ULL;
+                if (options.crlf) {
+                    uint64_t cr = simd_csv_cmp_mask_against_input(input, '\r');
+                    uint64_t cr_adjusted = (cr << 1) | prev_cr_end;
+                    uint64_t lf = simd_csv_cmp_mask_against_input(input, '\n');
+                    end = lf & cr_adjusted;
+                    prev_cr_end = cr >> 63;
+                } else {
+                    end = simd_csv_cmp_mask_against_input(input, '\n');
+                }
+                fields[b] = (end | delim) & ~quote_mask;
+            }
+
+            uint32_t count = 0;
+            for (size_t b = 0 ; b < SIMD_CSV_BUFFER_SIZE; b++) {
+                size_t internal_idx = 64 * b + idx;
+                simd_csv_flatten_bits(buffer_indexes, &count, internal_idx, fields[b]);
+            }
+            uint32_index_array_extend(csv, buffer_indexes, count);
+        }
+    }
+
+    for (; idx < len_last_64; idx += 64) {
+        #ifndef _MSC_VER
+        __builtin_prefetch(buffer + idx + 128);
+        #endif
+
+        simd_csv_input_t input = simd_csv_fill_input(buffer + idx);
+        uint64_t quote_mask = simd_csv_quote_mask(input, &prev_inside_quote);
+        uint64_t delim = simd_csv_cmp_mask_against_input(input, options.delimiter);
+        uint64_t end = 0ULL;
+        uint32_t indexes[64] = {0};
+        uint32_t count = 0;
+
+        if (options.crlf) {
+            uint64_t cr = simd_csv_cmp_mask_against_input(input, '\r');
+            uint64_t cr_adjusted = (cr << 1) | prev_cr_end;
+            uint64_t lf = simd_csv_cmp_mask_against_input(input, '\n');
+            end = lf & cr_adjusted;
+            prev_cr_end = cr >> 63;
+        } else {
+            end = simd_csv_cmp_mask_against_input(input, '\n');
+        }
+        uint64_t fields = (end | delim) & ~quote_mask;
+
+        if (fields != 0ULL) {
+            simd_csv_flatten_bits(indexes, &count, idx, fields);
+            uint32_index_array_extend(csv, indexes, count);
+        }
+    }
+
+    if (idx < len) {
+        uint8_t last_buffer[64] = {0};
+        memcpy(last_buffer, buffer + idx, len - idx);
+        simd_csv_input_t input = simd_csv_fill_input(last_buffer);
+        uint64_t quote_mask = simd_csv_quote_mask(input, &prev_inside_quote);
+        uint64_t delim = simd_csv_cmp_mask_against_input(input, options.delimiter);
+        
+        uint64_t end = 0ULL;
+        uint32_t indexes[64] = {0};
+        uint32_t count = 0;
+
+        if (options.crlf) {
+            uint64_t cr = simd_csv_cmp_mask_against_input(input, '\r');
+            uint64_t cr_adjusted = (cr << 1) | prev_cr_end;
+            uint64_t lf = simd_csv_cmp_mask_against_input(input, '\n');
+            end = lf & cr_adjusted;
+            prev_cr_end = cr >> 63;
+        } else {
+            end = simd_csv_cmp_mask_against_input(input, '\n');
+        }
+        uint64_t fields = (end | delim) & ~quote_mask;
+
+        if (fields != 0ULL) {
+            simd_csv_flatten_bits(indexes, &count, idx, fields);
+            uint32_index_array_extend(csv, indexes, count);
+        }
+    }
+    return true;
+}
 
 #endif
